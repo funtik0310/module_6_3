b304720a8f3cc4e5b0724d58e46e577d433c9c7d
funtik0310 <funtik03101996@gmail.com>
2024-09-26 16:05:24 +0300

    Update module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -24,8 +24,8 @@ class Pegasus(Horse, Eagle):
         super().__init__()
 
     def move(self, dx, dy):
-        super().run(dx)
-        super().fly(dy)
+        self.run(dx)
+        self.fly(dy)
 
     def get_pos(self):
         return self.x_distance, self.y_distance
